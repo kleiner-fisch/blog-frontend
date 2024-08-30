31bbcbba1ac424003dc325a9b6c09a754f0120ec
heinrich <heinrich.ody@gmail.com>
2024-08-30 16:21:20 +0800

    added a home landing site and trying to get deployment to azure working

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,32 @@
-import Button from '@mui/material/Button';
+import { Link} from 'react-router-dom';
 import  Typography  from '@mui/material/Typography';
 
 export function Home(){
-    return (<div> <Button>Click Me</Button><h1>Home</h1></div>);
+    return (<>
+        <Typography variant={'h1'}>Home</Typography>
+        <Typography variant="body1">
+            This is a small blog created to learn some web development skills.
+        </Typography>
+        <Typography variant="body1">
+            The blog data is downloaded from a publicly available data store, 
+            and made available via a RESTful API: 
+            <Link to='https://hody-blog.azurewebsites.net/blog/api/v1/swagger-ui/index.html'>
+                https://hody-blog.azurewebsites.net/blog/api/v1/swagger-ui/index.html
+            </Link> 
+        </Typography>
+
+        <Typography variant="body1">
+            The view is documented here:
+                <Link to='https://github.com/kleiner-fisch/blog-frontend'>
+                    https://github.com/kleiner-fisch/blog-frontend
+                </Link> 
+        </Typography>
+        <Typography variant="body1">
+            The blog API is documented here:  
+                <Link to='https://github.com/kleiner-fisch/blog'>
+                    https://github.com/kleiner-fisch/blog
+                </Link> 
+        </Typography>
+        <Link to={'./posts'}><h2>Blog</h2></Link>
+    </>);
 } 
